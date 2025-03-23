e67ff260facc02689f1e4413f2533e8a2b345437
RyuTatsukiSama <dorianribault@gmail.com>
2025-03-23 13:40:31 +0100

    Try to correct UI bug in MainMenu but didn't work

--- MainMenu/mainMenu.cpp
+++ MainMenu/mainMenu.cpp
@@ -9,7 +9,7 @@ STATE MainMenu::Update()
     while (choice < 0 || choice > 2)
     {
         std::cout << "Error ! Please enter a number gfrom the following options" << std::endl;
-        std::cout << "[1] Launch Random\n [2] Settings\n [0] Quit" << std::endl;
+        std::cout << "[1] Launch Random\n[2] Settings\n[0] Quit" << std::endl;
         std::cin >> choice;
     }
 
